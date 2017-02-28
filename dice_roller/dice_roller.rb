4ddfd5340c124202b7d583702d55e9a629c001c1
marekpolakowski <marek.polakowski@gmail.com>
2017-02-28 22:40:09 +0100

    dice_roller - 4

--- dice_roller/dice_roller.rb
+++ dice_roller/dice_roller.rb
@@ -1,10 +1,22 @@
 require_relative 'dice.rb'
 require_relative 'roller.rb'
 
-# rolls seven times with dice k-20
-# rejects maximum and minimum value
-# then rolls once again
+# rolls dice k-30 once before, and
+# if it returns (1..7), then rolls k-30 once again,
+# and if it returns 1, then not minmax is rejected, but 2 smallest numbers
 
+dice_k30 = Dice.new(30)
 dice_k20 = Dice.new(20)
-roller = Roller.new(dice_k20, 7)
+reject_method = if (1..7).include?(dice_k30.roll) && dice_k30.roll == 1
+                  puts 'TWO SMALLEST'
+                  :reject_two_smallest
+                else
+                  puts 'MIN MAX'
+                  :reject_minmax
+                end
+
+
+
+roller = Roller.new(dice_k20, 7, reject_method)
+
 puts roller.perform.inspect
--- dice_roller/roller.rb
+++ dice_roller/roller.rb
@@ -1,9 +1,10 @@
 class Roller
   attr_reader :dice, :rolls_count
 
-  def initialize(dice, rolls_count)
+  def initialize(dice, rolls_count, reject_method)
     @dice = dice
     @rolls_count = rolls_count
+    @reject_method = reject_method
   end
 
   def perform
@@ -12,9 +13,21 @@ class Roller
       result.push(dice.roll)
     end
     result.sort!
-    result.pop
-    result.shift
+    puts "rolled: #{result.inspect}"
+    send(@reject_method, result)
+    puts "after reject: #{result.inspect}"
     result.push(dice.roll)
     result
+  end
+
+  private
+
+  def reject_minmax(result)
+    result.pop
+    result.shift
+  end
+
+  def reject_two_smallest(result)
+    result.shift(2)
   end
 end
