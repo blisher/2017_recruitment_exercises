8fb8fd6edcb7d59a2b138e06ca16a36e4b28aafd
marekpolakowski <marek.polakowski@gmail.com>
2017-02-28 21:02:45 +0100

    car_factory - car.rb

---
+++ car_factory/car.rb
@@ -0,0 +1,29 @@
+class Car
+  COLORS = {
+    black: 'Black',
+    red: 'Red',
+    yellow: 'Yellow',
+    navy_blue: 'Navy Blue'
+  }
+
+  AVAILABLE_COLORS = %i(black red yellow)
+  @@last_color_index = 0
+
+  BRANDS = {
+    fiat: 'Fiat',
+    subaru: 'Subaru'
+  }
+
+  attr_reader :brand, :color_name
+
+  def initialize(brand, options = {})
+    @brand = BRANDS[brand]
+    @color_name = COLORS[options[:color]] || Car.generate_color
+  end
+
+  def self.generate_color
+    result = COLORS[AVAILABLE_COLORS[@@last_color_index]]
+    @@last_color_index = (@@last_color_index + 1) % AVAILABLE_COLORS.length
+    result
+  end
+end
